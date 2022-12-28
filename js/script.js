b2d865c41dc7ada2e32a7825e61c8686589f34d7
Multi <83913997+multicolor-rgb@users.noreply.github.com>
2022-12-28 15:12:31 +0100

    search added

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 fetch('https://GetSimpleCMS-CE-plugins.github.io/db.json')
-.then((response) => response.json())
-.then((data) => {
+    .then((response) => response.json())
+    .then((data) => {
 
-    data.forEach(el => {
+        data.forEach(el => {
 
 
-        document.querySelector('.db').insertAdjacentHTML('afterbegin', `
+            document.querySelector('.db').insertAdjacentHTML('afterbegin', `
 
 <div class="db-item">
 <h4>${el['name']}</h4>
@@ -19,7 +19,26 @@ fetch('https://GetSimpleCMS-CE-plugins.github.io/db.json')
 `)
 
 
+        });
+
+
     });
 
+
+document.querySelector('.title-search').addEventListener('keyup', (e) => {
+
+    document.querySelectorAll('.db-item').forEach(
+        x => {
+            x.style.display = "none";
+        }
+    );
+
+    document.querySelectorAll('.db-item').forEach(c=>{
+
+        if(c.querySelector('h4').innerHTML.toLowerCase().indexOf(document.querySelector('.title-search').value.toLowerCase()) > -1){
+            c.style.display="block";
+        }
+
+    })
 
 });
